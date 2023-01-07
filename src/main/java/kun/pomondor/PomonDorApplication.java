2b1af90245e350d138b576fcfe58e2d90426f84c
kun <sus5330@icloud.com>
2023-01-08 01:11:55 +0900

    start project

---
+++ src/main/java/kun/pomondor/PomonDorApplication.java
@@ -0,0 +1,13 @@
+package kun.pomondor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomonDorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomonDorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kun/pomondor/PomonDorApplicationTests.java
@@ -0,0 +1,13 @@
+package kun.pomondor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomonDorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
